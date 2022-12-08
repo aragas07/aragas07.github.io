5124b60a86ad34bd0bb2bfebb02ce7d5b66693f1
aragas08 <105354342+aragas08@users.noreply.github.com>
2022-12-09 03:55:54 +1200

    adding posting news

--- js/work.js
+++ js/work.js
@@ -1,5 +1,25 @@
-const app = Vue.createApp({
-  template: '<h1>Hello World</h1>',
-})
+function auto_grow(element) {
+  element.style.height = "5px";
+  element.style.height = (element.scrollHeight)+"px";
+}
 
-app.mount("#app")
+$(function(){
+  $("#add-section").click(function(){
+    $('<div class="cont"><div class="components"><div class="component plus"></div><div class="minus"></div><div class="close"></div></div><div class="section"><div class="element"><di class="plus"></di></div><div class="element"><di class="plus"></di></div></div></div>')
+    .insertBefore("#add-section");
+    $(".component").each(function(e){
+      $(this).click(function(){
+        $(".section").eq(e).append('<div class="element"><di class="plus"></di></div>');
+      })
+    })
+    $(".minus").click(function(){
+      $(".element").eq(0).remove();
+    })
+  })
+  $(".component").click(function(){
+    $(".section").eq(0).append('<div class="element"><di class="plus"></di></div>');
+  })
+  $(".minus").click(function(){
+    $(".element").eq(0).remove();
+  })
+})
